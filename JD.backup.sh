153a55fbad5699f4d98f33a4563895d4e510e0b4
yisheng <jimmy@yisheng>
2015-06-01 16:09:04 +0800

    backup

--- JD.backup.sh
+++ JD.backup.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
-cd /WORK/HTML
 
-echo 'backup html data ...'
-tar czvf redbull_`date +%Y%m%d`.tar.gz redbullsports.cn
-scp redbull_`date +%Y%m%d`.tar.gz RD03:/WORK/BACKUP/
-rm redbull_`date +%Y%m%d`.tar.gz -f;
-ssh rd03 "cd /WORK/BACKUP;find -atime +14 -delete"
+#BACKUP DB --> web01
+ssh db01 "cd /WORK;/bin/tar -czvf /WORK/BACKUP/DB_`date +%Y%m%d`.tar.gz DB"
+scp db01:/WORK/BACKUP/DB_`date +%Y%m%d`.tar.gz web01:/WORK/BACKUP/
+ssh db01 "rm /WORK/BACKUP/DB_`date +%Y%m%d`.tar.gz -f"
 
-echo 'backup mysql data ...'
-ssh rd02 "cd /WORK/DB;tar czvf server30_`date +%Y%m%d`.tar.gz server30"
-scp rd02:/WORK/DB/server30_`date +%Y%m%d`.tar.gz .
-scp server30_`date +%Y%m%d`.tar.gz rd04:/WORK/BACKUP
-rm server30_`date +%Y%m%d`.tar.gz -f
-ssh rd02 "cd /WORK/DB;rm server30_`date +%Y%m%d`.tar.gz -f"
+#BACKUP WWW --> web02
+cd /WORK
+/bin/tar -czvf /WORK/BACKUP/WWW_`date +%Y%m%d`.tar.gz /home/wwwroot
+scp /WORK/BACKUP/WWW_`date +%Y%m%d`.tar.gz web02:/WORK/BACKUP
+rm /WORK/BACKUP/WWW_`date +%Y%m%d`.tar.gz -f
 
-echo 'backup src data ...'
-ssh rd03 "cd /WORK/HTML;tar czvf src_`date +%w`.tar.gz src"
-scp rd03:/WORK/HTML/src_`date +%w`.tar.gz /WORK/BACKUP/
+#BACKUP upload --> web02
+ssh web02 "/bin/tar -czvf /WORK/BACKUP/upload_`date +%Y%m%d`.tar.gz /WORK/HTML/upload"
+
+#BACKUP HTML --> db01
+ssh db01 "cd /WORK;/bin/tar -czvf /WORK/BACKUP/HTML_`date +%Y%m%d`.tar.gz HTML"
+#scp db01:/WORK/BACKUP/HTML_`date +%Y%m%d`.tar.gz /WORK/BACKUP/
+#ssh db01 "rm /WORK/BACKUP/HTML_`date +%Y%m%d`.tar.gz -f"
+
+#BACKUP SVN --> db01
+cd /WORK
+/bin/tar -czvf /WORK/BACKUP/SVN_`date +%Y%m%d`.tar.gz /WORK/SVN
+scp /WORK/BACKUP/SVN_`date +%Y%m%d`.tar.gz web01:/WORK/BACKUP
+rm /WORK/BACKUP/SVN_`date +%Y%m%d`.tar.gz -f
+
+
+#TODO:: apache/php/nginx ...
+
+#Clean Old
+cd /WORK/BACKUP
+/bin/find . -ctime +14 | /usr/bin/xargs rm -Rf
+ssh db01 "cd /WORK/BACKUP;/bin/find . -ctime +4 | /usr/bin/xargs rm -Rf"
+ssh web02 "cd /WORK/BACKUP;/bin/find . -ctime +1 | /usr/bin/xargs rm -Rf"
 
