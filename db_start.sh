8a4bc7f4d20eff0a5aa18a2db690a8c47ecbb240
yisheng <jimmy@yisheng>
2015-09-14 17:01:13 +0800

    update

--- db_start.sh
+++ db_start.sh
@@ -12,4 +12,4 @@ fi
 
 echo "Start mysql at $rundir"
 #/usr/local/mysql/bin/mysqld_safe --defaults-file="$rundir"/my.cnf  --pid-file="$rundir"/mysql.pid --datadir="$rundir"/var --socket="$rundir"/mysql.sock --log-error="$rundir"/err.log --log-slow-queries="$rundir"/slow_query.log --log-bin="$rundir"/var/mysql-bin &
-/usr/local/mysql/bin/mysqld_safe --defaults-file="$rundir"/my.cnf  --pid-file="$rundir"/mysql.pid --datadir="$rundir"/var --socket="$rundir"/mysql.sock --log-error="$rundir"/err.log --slow-query-log-file="$rundir"/slow_query.log --log-bin="$rundir"/var/mysql-bin &
+/usr/local/mysql/bin/mysqld_safe --defaults-file="$rundir"/my.cnf  --pid-file="$rundir"/mysql.pid --datadir="$rundir"/var --socket="$rundir"/mysql.sock --log-error="$rundir"/err.log --slow-query-log --slow-query-log-file="$rundir"/slow_query.log --log-bin="$rundir"/var/mysql-bin &
