6ac0a447430d74e688a889ec7626ee3a27eef6bc
yisheng <jimmy@yisheng>
2016-02-26 11:44:26 +0800

    upgrage

--- apache_log.sh
+++ apache_log.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-goaccess -f /WORK/LOG/apache_logs/access_log.m.`date +%Y%m%d` --no-color --no-progress
+if [ "$1" == "" ]; then
+log='m'
+else
+log="$1"
+fi
+
+if [ "$2" == ""]; then
+date=".`date +%Y%m%d`"
+else
+date=""
+fi 
+
+goaccess -f /WORK/LOG/apache_logs/access_log.$log$date --no-color --no-progress
