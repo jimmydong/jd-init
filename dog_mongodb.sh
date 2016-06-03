6b471636246100ef179e6b402335150d84b13f93
yisheng <jimmy@yisheng>
2016-06-03 18:01:28 +0800

    add dog mongo

---
+++ dog_mongodb.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+log=/WORK/important_log
+date=`date`
+times=`/bin/ps aux | grep mongo | awk '{print $11}' | grep -v "grep"`
+echo $times
+if [ -z $times ]
+then
+	echo "dog_mongodb: mongodb not run at $date" >> $log
+	/WORK/SBIN/mongodb_start.sh	
+fi
--- mongodb_start.sh
+++ mongodb_start.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 if [ "$1" == "" ]; then
-echo -e "Usage: mongodb_start.sh DBNAME \neg:  mongodb_start.sh server0 \n!!!Important: DBNAME must be placed in /WORK/MONGODB!!!\n\n"
-exit
+rundir="/WORK/MONGO/"
+else
+rundir="/WORK/MONGO/$1"
 fi
-rundir="/WORK/MONGODB/$1";
+echo -e "Asert mongodb.conf is $rundir/mongodb.conf\n\n"
 if [ ! -d $rundir ];then
-echo -e "Error: Can not find the directory /WORK/MONGODB/$1 \n"
+echo -e "Error: Can not find the directory $rundir \n"
 exit
 fi
-
-
-echo "Start mysql at $rundir"
+echo "Start MongoDB at $rundir ..."
 /usr/local/mongodb/bin/mongod -f "$rundir"/mongodb.conf &
+echo "/usr/local/mongodb/bin/mongod -f $rundir/mongodb.conf &"
+echo "Done!"
