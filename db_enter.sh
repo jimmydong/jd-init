d522743b15952ffc0b7d9c91a99fda02fd74eef3
yisheng <jimmy@yisheng>
2017-06-06 15:16:45 +0800

    update

--- db_enter.sh
+++ db_enter.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# php2.7 install mycli
+
 #By jimmydong 20071108
 if [ "$1" == "" ]; then
 echo -e "Usage: db_enter.sh DBNAME \neg:  db_enter.sh server0 \n!!!Important: DBNAME must be placed in /WORK/DB!!!\n\n"
@@ -12,7 +14,8 @@ fi
 
 echo "Enter mysql at $rundir"
 if [ $# -lt 2 ]; then
-/usr/local/mysql/bin/mysql  --socket="$rundir"/mysql.sock -u root -p`cat $rundir/mysql.security.ini`
+mycli  --socket="$rundir"/mysql.sock -u root -p`cat $rundir/mysql.security.ini`
+#/usr/local/mysql/bin/mysql  --socket="$rundir"/mysql.sock -u root -p`cat $rundir/mysql.security.ini`
 #/usr/local/mysql/bin/mysql  --socket="$rundir"/mysql.sock -u root
 else
 /usr/local/mysql/bin/mysql  --socket="$rundir"/mysql.sock $2 $3 $4
